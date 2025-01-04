58dc73470c4b957ad0cebe5ef6f6e14da77e5b88
kplnhsyn <kplnhsyn.49@gmail.com>
2025-01-04 16:57:38 +0100

    readme.md and header.jsx updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import { contacts } from "./data";
 import "bootstrap/dist/css/bootstrap.css";
+import "./styles.css";
 // console.log(contacts);
 
 const App = () => {
     return (
         <div className="row justify-content-center gap-1 m-3">
-            <h1 className="text-danger text-center m-4 border-bottom display-1">
-                FullVitaminStack Team
-            </h1>
+            <Header />
             {contacts.map((item) => {
                 const { id, name, imgURL, phone, email } = item;
                 return (
